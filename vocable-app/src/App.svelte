6c3954ae9db4fe2d8ee57d233cd5fd2757841386
AzureWings123 <raganvanessa@gmail.com>
2022-03-04 22:03:53 -0800

    🌿commit letter check
    🐜: While it can tell if the letter is right, wrong, or close, there is a bug in the close. It only checks if the correct letter is before where it should be. If it comes after where it should be, it gets marked wrong.

--- vocable-app/src/App.svelte
+++ vocable-app/src/App.svelte
@@ -2,20 +2,48 @@
 	export let name;
 
 	let guess = '';
+	$: guessLength = guess.length - 1;
 
 	let correct_guess = "wordle"
+	let letter_state = '';
+
+	function checkLetters(str1, str2) 
+		{
+		for (let i=0;i<guess.length; i++){
+			if(guess[guessLength] != correct_guess[i]){
+				for(let j=0; j < correct_guess.length; j++){
+					if(guess[guessLength] == correct_guess[j]){
+						letter_state = "close"
+					}
+					else {
+						letter_state = "wrong"
+					}
+				}
+			}
+			else{
+				letter_state = "right"
+			}
+		}
+	}
+	
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<input bind:value={guess} placeholder='Enter Guess'/>
 	<h3>Guess: {guess}</h3>
+	{checkLetters(guess, correct_guess)}
+	{letter_state}
+
 	{#if guess == correct_guess}
 	<h4 style="color: green">
 		Your guess is correct!
 	</h4>
 	{/if}
+
+
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
 </main>
 
 <style>
